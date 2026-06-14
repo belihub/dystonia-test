639ac212fbac41e7d76ad47bcde929f4f806edee
beli <imnutsdoc@gmail.com>
2026-06-14 17:40:45 +0530

    add github action to publish to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'beli.github.io',
+    base: '/astro-dystonia-test'
+});
