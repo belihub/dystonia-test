e893dd7a5245852424bee7bddc64b7786b78262a
beli <imnutsdoc@gmail.com>
2026-06-14 18:44:16 +0530

    Add preceding and trailing slashes to the 'base' value in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://belihub.github.io',
-    base: 'dystonia-test',
+    base: '/dystonia-test/',
 });
